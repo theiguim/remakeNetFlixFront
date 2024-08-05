1eeba5f2883d6e1d16d6243c4151ef90b2042a2a
theiguim <theiguim@gmail.com>
2024-08-05 19:36:07 -0300

    slides comp

---
+++ src/services/courseService.ts
@@ -0,0 +1,34 @@
+import { error } from "console";
+import api from "./api";
+
+export type EpisodeType = {
+    id: number,
+    name: string,
+    synopsis: string,
+    order: number,
+    videUrl: string,
+    secondsLong: number
+}
+
+export type CourseType = {
+    id:number,
+    name: string, 
+    thumbnailUrl: String,
+    synopsis: string,
+    episodes?: EpisodeType[]
+}
+
+const courseService = {
+
+    getNewestCourses: async ()=>{
+        const res = await api.get("/courses/newest").catch((error)=>{
+            console.log(error.response.data.message);
+
+            return error.response
+        });
+        return res;
+    },
+
+};
+
+export default courseService
