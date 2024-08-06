75a8284d0b4822d9828b8290c22ceda6ace41e80
theiguim <theiguim@gmail.com>
2024-08-05 22:02:05 -0300

    finish footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import SlideSection from "../src/components/homeNoAuth/slideSection";
 import { GetStaticProps } from "next";
 import courseService, { CourseType } from "../src/services/courseService";
 import { ReactNode } from "react";
+import Footer from "../src/components/common/footer";
 
 
 interface IndexPageProps {
@@ -32,6 +33,7 @@ const HomeNoAuth = ({course}: IndexPageProps) =>{
      </div>
      <CardsSection />
      <SlideSection newestCourses={course}/>
+     <Footer />
     </main>
     </>
   )
