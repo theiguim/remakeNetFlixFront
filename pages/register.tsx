b9d3f8522ea8df6af823b35884deacb3014b39a6
theiguim <theiguim@gmail.com>
2024-08-06 21:12:42 -0300

    modal

--- pages/register.tsx
+++ pages/register.tsx
@@ -3,7 +3,7 @@ import HeaderGeneric from "../src/components/common/headerGeneric";
 import styles from "../styles/registerLogin.module.scss";
 import Head from "next/head";
 import { Form, FormGroup, Label, Input, Container, Button } from "reactstrap";
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import authService from "../src/services/authService";
 import { useRouter } from "next/router";
 import ToastComponent from "../src/components/common/toast";
@@ -15,6 +15,12 @@ const Register = () => {
     const router = useRouter()
     const [toastIsOpen, setToastIsOpen] = useState(false);
     const [toastMessage, setToastMessage] = useState("");
+
+    useEffect(()=>{
+        if(sessionStorage.getItem("onebitflix-token")){
+            router.push("/home")
+        }
+    }, [])
 
     const handleRegister = async (event: FormEvent<HTMLFormElement>)=>{
         event.preventDefault();
