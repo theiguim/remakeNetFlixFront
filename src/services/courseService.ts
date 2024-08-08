f53a209f143a2a56df78ee9f984d1b0949ae2a14
theiguim <theiguim@gmail.com>
2024-08-07 23:44:16 -0300

    criacao search

--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -59,11 +59,11 @@ const courseService = {
     const token = sessionStorage.getItem("onebitflix-token");
 
     const res = await api
-    .delete ("/favorites", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      }
-    })
+      .delete("/favorites", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        }
+      })
       .catch((error) => {
         console.log(error.response.data.message);
 
@@ -88,7 +88,26 @@ const courseService = {
       });
 
     return res;
+  },
+
+  getSearch: async (name: string) => {
+
+    const token = sessionStorage.getItem("onebitflix-token");
+
+    const res = await api
+      .get(`/courses/search?name=${name}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .catch((error) => {
+
+        return error.response;
+      });
+
+    return res;
   }
+
 };
 
 export default courseService
